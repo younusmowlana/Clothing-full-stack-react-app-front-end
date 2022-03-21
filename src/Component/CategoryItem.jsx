553c435a198ce85cf824c668babadeb26b970de6
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-03-21 17:40:09 +0530

    Categories-all-done

--- src/Component/CategoryItem.jsx
+++ src/Component/CategoryItem.jsx
@@ -1,25 +1,56 @@
+import styled from "styled-components";
+import { mobile } from "../responsive";
 
-import styled from 'styled-components';
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`;
 
-const Container = styled.div``;
-const Image = styled.img``;
-const Info = styled.div``;
-const Title = styled.h1``;
-const Button = styled.button``;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  ${mobile({ height: "20vh" })}
+`;
 
-const CategoryItem = ({item}) => {
-  console.log()
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+    color:white;
+    margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+    border:none;
+    padding: 10px;
+    background-color: white;
+    color:gray;
+    cursor: pointer;
+    font-weight: 600;
+`;
+
+const CategoryItem = ({ item }) => {
   return (
-    <Container key={item.id}>
-      
-      <Image src={item.img}/>
+    <Container>
+      <Image src={item.img} />
       <Info>
         <Title>{item.title}</Title>
         <Button>SHOP NOW</Button>
       </Info>
-
     </Container>
-  )
-}
+  );
+};
 
-export default CategoryItem
+export default CategoryItem;
