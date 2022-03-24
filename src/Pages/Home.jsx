f7fb1d898ffa94e06cacd0f967a22fea17d9e4d8
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-03-24 15:12:38 +0530

    Footer-design

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Announcement from '../Component/Announcement'
 import Categories from '../Component/Categories'
 import Navbar from '../Component/Navbar'
+import Newsletter from '../Component/Newsletter'
 import Products from '../Component/Products'
 import Slider from '../Component/Slider'
 
@@ -14,6 +15,7 @@ function Home() {
       <Slider/>
       <Categories/>
       <Products/>
+      <Newsletter/>
     </div>
   )
 }
