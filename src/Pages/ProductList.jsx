7eb3031ec4207bd085e4e421bc42d35ca5f8dbac
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-03-24 15:21:57 +0530

    new-page-added

---
+++ src/Pages/ProductList.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components"
+
+const Container=styled.div``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default ProductList
