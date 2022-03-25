f533ef833551fea89916af3abaaa5b6b06925a0c
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-03-26 00:14:01 +0530

    front-end-updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Cart from "./Pages/Cart";
-
-
+ import Home  from "./Pages/Home";
+// import Login from "./Pages/Login"
 
 const App = () => {
   return (<>
-  <Cart/>
+  <Home/>
    </>
   );
 };
