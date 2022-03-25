ae6b95daef17c2a2436b4f9428449fab3bcc5655
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-03-25 19:44:09 +0530

    Login-updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Home  from "./Pages/Home";
+import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 
 
 const App = () => {
   return (<>
-  <Register/>
+  <Login/>
    </>
   );
 };
