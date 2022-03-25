ce0a8e1ac5d4a13b7d26f52e68434672aa0a7ee6
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-03-26 00:13:13 +0530

    Navbar-reupdated

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,95 +1,97 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+import { mobile } from "../responsive";
 
-import { Badge } from '@material-ui/core';
-import { Search, ShoppingCartOutlined } from '@material-ui/icons';
-import React from 'react'
-import styled from 'styled-components';
-
-const Container =styled.div`
- height:60px;
+const Container = styled.div`
+  height: 60px;
+  ${mobile({ height: "50px" })}
 `;
 
-const Wrapper= styled.div`
- padding: 10px 20px;
- display: flex;
- align-items: center;
- justify-content: space-between;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
 `;
 
-const Left=styled.div`
- flex: 1;
- display: flex;
- align-items:center;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
-const Languages=styled.span`
- font-size: 14px;
- cursor: pointer;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
 
-const SearchContainer=styled.div`
- border: 0.5px solid lightgray;
- display: flex;
- align-items: center;
- margin-left: 25px;
- padding: 5px;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
 `;
 
-const Input=styled.input`
- border: none;
+const Input = styled.input`
+  border: none;
+  ${mobile({ width: "50px" })}
 `;
 
-const Center=styled.div`
- flex: 1;
- text-align: center;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
 `;
 
-const Logo=styled.h1`
- font-weight: bold;
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
 `;
 
-
-const Right=styled.div`
- flex: 1;
- display: flex;
- align-items: center;
- justify-content: flex-end;
- 
-`;
-const Menuitem= styled.div`
- font-size: 14px;
- cursor: pointer;
- margin-left: 25px;
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
 const Navbar = () => {
   return (
     <Container>
-        <Wrapper>
-
+      <Wrapper>
         <Left>
-            <Languages>En </Languages> 
-            <SearchContainer>
-                <Input/>
-                <Search style={{color:"gray",fontSize:16}}/>
-            </SearchContainer>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
         </Left>
-        <Center><Logo>SHARP.</Logo></Center>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
         <Right>
-            <Menuitem>REGISTER</Menuitem>
-            <Menuitem>SIGN IN</Menuitem>
-            <Menuitem>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
             <Badge badgeContent={4} color="primary">
-<ShoppingCartOutlined/>
-    </Badge>
-            
-            </Menuitem>
-
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
         </Right>
-
-        </Wrapper>
-      
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
