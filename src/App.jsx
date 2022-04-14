833374d3e3aa032b5d264f9e777b76ef0b4db50c
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-15 03:54:52 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
- import Home  from "./Pages/Home";
- import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-// import Login from "./Pages/Login"
+//  import Home  from "./Pages/Home";
+//  import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
+// // import Login from "./Pages/Login"
 
-const App = () => {
-  return (<>
-  <Home/>
-   </>
-  );
-};
+// const App = () => {
+//   return (<>
+//   <Home/>
+//    </>
+//   );
+// };
 
-export default App;
+// export default App;
