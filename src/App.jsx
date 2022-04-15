73ad4f9c8188f0d3b531b5b63a03b4896800cfbd
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-16 03:58:06 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
  import Home  from "./Pages/Home";
 // import Login from "./Pages/Login"
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import ProductList from "./Pages/ProductList";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route  path="/" >
+        <Route exact path="/" >
           <Home/>
+        </Route>
+        <Route  path="/products" >
+         <ProductList/>
         </Route>
       </Switch>
     </Router>
