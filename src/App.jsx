ecb609aea9cbd310f1a9156337d1531e4b4c9e4f
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-16 04:08:44 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Product from "./Pages/Product";
 import Home from "./Pages/Home";
 import ProductList from "./Pages/ProductList";
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
+// import Register from "./Pages/Register";
+// import Login from "./Pages/Login";
 import Cart from "./Pages/Cart";
 import {
   BrowserRouter as Router,
