2e212740fc5ea5eeafe354cf67f58e5d970ad21d
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-16 14:35:14 +0530

    Updated

--- src/Component/CategoryItem.jsx
+++ src/Component/CategoryItem.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   flex: 1;
