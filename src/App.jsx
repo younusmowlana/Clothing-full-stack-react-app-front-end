a35bef6ee3073d79a9c7f4cfd5be9042e9b96148
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-16 13:56:25 +0530

    React-routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Product from "./Pages/Product";
 import Home from "./Pages/Home";
 import ProductList from "./Pages/ProductList";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 // import Register from "./Pages/Register";
 // import Login from "./Pages/Login";
 import Cart from "./Pages/Cart";
@@ -26,6 +28,12 @@ const App = () => {
         </Route>
         <Route path="/cart">
           <Cart />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
         </Route>
       </Switch>
     </Router>
