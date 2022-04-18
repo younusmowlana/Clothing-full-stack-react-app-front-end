20994ff23ff5d6cb97d05e7f88b513373b25bf36
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-18 16:48:25 +0530

    Updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import React,{useEffect} from 'react'
+// import axios from 'axios'
+import React from 'react'
 import Announcement from '../Component/Announcement'
 import Categories from '../Component/Categories'
 import Footer from '../Component/Footer'
@@ -10,12 +10,12 @@ import Slider from '../Component/Slider'
 
 
 function Home() {
-  useEffect(async()=>{
+  // useEffect(async()=>{
 
-    const response =await axios.get("http://localhost:8000/api/test")
-    console.log(response.data)
+  //   const response =await axios.get("http://localhost:8000/api/test")
+  //   console.log(response.data)
 
-  },[])
+  // },[])
   return (
     <div>
       <Announcement/>
--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -40,7 +40,7 @@ const Option = styled.option``;
 
 const ProductList = () => {
   const location = useLocation();
-  const cat = location.pathname.split("/") [2];
+  const cat = location.pathname.split("/")[2];
   const [filters,setFilters] = useState({});
   const [sort,setSort] = useState("newst");
 
