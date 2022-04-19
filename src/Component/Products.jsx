3feb639525d247ddc20bbe28bb43faeea6bb1521
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-19 23:27:56 +0530

    Updated

--- src/Component/Products.jsx
+++ src/Component/Products.jsx
@@ -16,6 +16,9 @@ const Products = (cat,filters,sort) => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
+ 
+
+
   useEffect(()=>{
     const getProducts = async () => {
       try{
