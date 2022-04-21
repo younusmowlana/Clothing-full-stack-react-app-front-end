dde47e999b654b05b9a6c70ca7f4820c2d16ede0
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-21 23:23:24 +0530

    filtering-products

--- src/Component/Products.jsx
+++ src/Component/Products.jsx
@@ -5,56 +5,64 @@ import Product from "./Product";
 import axios from "axios";
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
-const Products = (cat,filters,sort) => {
-    console.log(cat,)
+const Products = ({ cat, filters, sort }) => {
   const [products, setProducts] = useState([]);
-  console.table(products)
   const [filteredProducts, setFilteredProducts] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const getProducts = async () => {
-      try{
+      try {
         const res = await axios.get(
-          cat 
-        ? `http://localhost:5000/api/product?category=${cat.cat}`
-        : `http://localhost:5000/api/product`);
-        // console.log(res.data)
-       setProducts(res.data);
-
-      }catch(err){
-      }
+          cat
+          ? `http://localhost:5000/api/product?category=${cat}`
+          : `http://localhost:5000/api/product`);
+        setProducts(res.data);
+      } catch (err) {}
     };
-
     getProducts();
-  },[cat])
-  useEffect(() =>{
-    cat.cat && setFilteredProducts(
-      products.filter(item=> Object.entries(filters).every(([key,value])=>
-      item[key].includes(value)
-      
-      ))
-    );
+  }, [cat]);
 
-  },[products,cat,filters])
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
 
-// console.log(filteredProducts)
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
 
   return (
     <Container>
-      {filteredProducts.map((item) => (
-        <Product item={item} key={item.id} />
-      ))}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
     </Container>
-      // {popularProducts.map((item) => (
-      //   <Product item={item} key={item.id} />
-      // ))}
-
   );
 };
 
