c7b06bcbc67b12e9a145dc08f551f2b48900b537
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-21 23:12:41 +0530

    Updated

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -64,12 +64,12 @@ const ProductList = () => {
             <Option disabled >
               Color
             </Option>
-            <Option>White</Option>
-            <Option>Black</Option>
-            <Option>Red</Option>
-            <Option>Blue</Option>
-            <Option>Yellow</Option>
-            <Option>Green</Option>
+            <Option>white</Option>
+            <Option>black</Option>
+            <Option>red</Option>
+            <Option>blue</Option>
+            <Option>yellow</Option>
+            <Option>green</Option>
           </Select>
           <Select  name="size" onChange={handleFilters}>
             <Option disabled >
