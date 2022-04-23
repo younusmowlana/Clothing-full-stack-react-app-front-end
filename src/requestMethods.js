05b9c4c2b4bd8ff1fde10917581be3cd75c63534
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-24 02:30:09 +0530

    Testing token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 
 const BASE_URL = "http://localhost:5000/api/";
+const TOKEN = 
+    JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+     .accessToken || "";
 
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
