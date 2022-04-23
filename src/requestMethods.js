842a56e8d25144ac120f42241a28ca6224fd1fe9
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-24 02:25:04 +0530

    Updated

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -5,3 +5,6 @@ const BASE_URL = "http://localhost:5000/api/";
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
 });
+export const privateRequest = axios.create({
+    baseURL:BASE_URL,
+});
