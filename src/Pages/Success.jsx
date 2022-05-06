8c72ff5541bfc9d135487e423aa1dec2af53a22d
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-06 09:29:33 +0530

    Updated

---
+++ src/Pages/Success.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useLocation } from "react-router";
+
+const Success = () => {
+    const location = useLocation();
+    console.log(location)
+  return (
+    <div>
+      Successfull !!
+    </div>
+  )
+}
+
+export default Success
