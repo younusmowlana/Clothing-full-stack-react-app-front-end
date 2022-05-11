c639906c42fb1e63f72f024dbc9947d6eae62674
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-11 17:09:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ import {
   Redirect,
 } from "react-router-dom";
 import Success from "./Pages/Success";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  const user = true;
+  const user = useSelector((state)=>state.user.currentUser);
   return (
     <Router>
       <Switch>
