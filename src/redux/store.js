848b2a299c2f55d29f8a691c06966aadfa86c559
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-11 15:45:39 +0530

    Updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore} from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
+import userReducer from "./userRedux";
+
+
 // import userReducer from "./userRedux";
 //import {composeWithDevTools} from '@redux-devtools/extension'
 
@@ -7,5 +10,6 @@ export default configureStore({
   
   reducer: {
     cart: cartReducer,
+    user: userReducer,
   },
 })
