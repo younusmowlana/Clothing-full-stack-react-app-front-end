45b3aafeb067e7682e7a08c021195e05b7afd372
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-11 15:39:46 +0530

    updated

---
+++ src/redux/userRedux.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    currentUser: null,
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    addProduct: (state, action) => {
+      state.quantity += 1;
+      state.products.push(action.payload);
+      state.total += action.payload.price * action.payload.quantity;
+    },
+  },
+});
+
+export const { addProduct } = userSlice.actions;
+export default userSlice.reducer;
