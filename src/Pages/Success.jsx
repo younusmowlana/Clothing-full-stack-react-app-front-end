b9b37e80b00c013b151daacda7ba03df8ac81759
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-25 01:44:07 +0530

    Updated

--- src/Pages/Success.jsx
+++ src/Pages/Success.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+import { userRequest } from "../requestMethods";
 
 const Success = () => {
-    const location = useLocation();
-    console.log(location)
-  return (
-    <div>
-      Successfull !!
-    </div>
-  )
-}
+  const location = useLocation();
+  //in Cart.jsx I sent data and cart. Please check that page for the changes.(in video it's only data)
+  const data = location.state.stripeData;
+  const cart = location.state.cart;
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const [orderId, setOrderId] = useState(null);
 
-export default Success
+  useEffect(() => {
+    const createOrder = async () => {
+      try {
+        const res = await userRequest.post("/order", {
+          userId: currentUser._id,
+          products: cart.products.map((item) => ({
+            productId: item._id,
+            quantity: item._quantity,
+          })),
+          amount: cart.total,
+          address: data.billing_details.address,
+        });
+        setOrderId(res.data._id);
+      } catch {}
+    };
+    data && createOrder();
+  }, [cart, data, currentUser]);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {orderId
+        ? `Order has been created successfully. Your order number is ${orderId}`
+        : `Successfull. Your order is being prepared...`}
+        <Link to="/">
+      <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Success;
