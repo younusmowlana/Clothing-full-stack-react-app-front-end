962c82a58af3a56ad9c0a062268039fbfe494791
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-26 18:37:35 +0530

    Updated

--- src/redux/userRedux.js
+++ src/redux/userRedux.js
@@ -1,9 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+const authUser = localStorage.getItem("currentUser")
+const user = JSON.parse(authUser)
+console.log(user)
 const userSlice = createSlice({
+  
   name: "user",
   initialState: {
     currentUser: null,
+    user: [],
     isFetching: false,
     error: false,
   },
@@ -22,8 +26,24 @@ const userSlice = createSlice({
     logout: (state) => {
       state.currentUser = null;
     },
+
+    //Register
+    addUserStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    addUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.user.push(action.payload);
+      state.currentUser = action.payload;
+    },
+    addUserFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export const { loginStart, loginSuccess, loginFailure, addUserStart, addUserSuccess,  addUserFailure} = userSlice.actions;
 export default userSlice.reducer;
