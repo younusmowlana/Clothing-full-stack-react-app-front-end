82948d542ad13e59810f2c63e6062b4d7650227b
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-26 18:37:27 +0530

    Updated

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,12 +1,25 @@
-import { loginFailure, loginStart, loginSuccess } from "./userRedux";
-import { publicRequest } from "../requestMethods";
+import { loginFailure, loginStart, loginSuccess, addUserStart, addUserSuccess, addUserFailure} from "./userRedux";
+import { publicRequest,userRequest } from "../requestMethods";
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
     const res = await publicRequest.post("/auth/login", user);
     dispatch(loginSuccess(res.data));
+    localStorage.setItem("currentUser",JSON.stringify(res.data))
   } catch (err) {
     dispatch(loginFailure());
+  }
+};
+export const addUser = async (dispatch, user) => {
+  dispatch(addUserStart());
+  try {
+    console.log("user")
+    console.log(user)
+    const res = await userRequest.post(`/user`, user);
+    dispatch(addUserSuccess(res.data));
+    localStorage.setItem("currentUser",JSON.stringify(res.data))
+  } catch (err) {
+    dispatch(addUserFailure());
   }
 };
