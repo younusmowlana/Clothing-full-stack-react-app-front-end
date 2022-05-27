636db54a5ff19ff8c335b7ec643d13a77ad34c7b
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-28 00:15:05 +0530

    Updated

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -5,7 +5,8 @@ const BASE_URL = "http://localhost:5000/api/";
 //     JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //      .accessToken || "";
 //const TOKEN =  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
+const TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDYwMTZhNjE2YWQ2YzkxOTNlY2FlNiIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NTM2NjIxODIsImV4cCI6MTY1MzkyMTM4Mn0.LZZA3f1fmcCy8QEd5bMyieEp8y00nSLgHD2j_Z3ePF4";
+
 
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
